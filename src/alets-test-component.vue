a5f1bd8c9aca881e7be0c9d8eb90140ea8a7fedc
Alejandro Martínez <aletsmart@pop-os.localdomain>
2021-05-07 01:53:01 -0500

    final structure

---
+++ src/alets-test-component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    {{ games }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HelloWorld',
+
+  data() {
+    return {
+      games: [],
+    }
+  },
+
+  async mounted() {
+    this.games = await axios.get(
+      'https://8tvo5bp9ai.execute-api.us-west-2.amazonaws.com/prod/gamesBySeason/1325',
+      {},
+    )
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
