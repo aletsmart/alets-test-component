96bfbde72981d8a2620c24064ef5e83d6a7daf54
Alejandro Martínez <aletsmart@pop-os.localdomain>
2021-05-07 02:25:13 -0500

    add node resolve package

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+import { nodeResolve } from '@rollup/plugin-node-resolve'; // Includes external dependencies
 export default {
   input: 'src/wrapper.js', // Path relative to package.json
   output: {
@@ -13,6 +14,7 @@ export default {
       css: true, // Dynamically inject css as a <style> tag
       compileTemplate: true, // Explicitly convert template to render function
     }),
-    buble(), // Transpile to ES5
+    nodeResolve(),
+    buble({ transforms: { asyncAwait: false } }), // Transpile to ES5
   ],
 };
